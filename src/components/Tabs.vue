bebbc149bbb0a0c6a786a2b0c67c4daa0a89c6fe
Gerda Siitan <gerda.siitan13@gmail.com>
2023-12-03 20:33:24 +0200

    OMGGGGGGG

---
+++ src/components/Tabs.vue
@@ -0,0 +1,30 @@
+<template>
+    <ul class="nav nav-tabs">
+        <li class="nav-item">
+            <router-link class="nav-link" to="/" activeClass="active">Home</router-link>
+        </li>
+        <li class="nav-item">
+            <router-link class="nav-link" to="/ToDo" activeClass="active">ToDo</router-link>
+        </li>
+        <li class="nav-item">
+            <router-link class="nav-link" to="/Clicker" activeClass="active">Clicker</router-link>
+        </li>
+        <li class="nav-item">
+            <router-link class="nav-link" to="/RickandMorty" activeClass="active">RickandMorty</router-link>
+        </li>
+        <li class="nav-item">
+            <router-link class="nav-link" to="/GoogleMaps" activeClass="active">googlemaps</router-link>
+        </li>
+        
+        <li class="nav-item">
+            <router-link class="nav-link" to="/Leaflet" activeClass="active">Leaflet</router-link>
+        </li>
+        
+    </ul>
+</template>
+
+<script>
+export default {
+
+}
+</script>
---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import fs from 'fs';
+import { PurgeCSSPlugin } from 'purgecss-webpack-plugin';
+import { VueLoaderPlugin } from 'vue-loader';
+import Dotenv from 'dotenv-webpack';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+let views = fs.readdirSync('./src/views')
+
+export default {
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'public')
+        },
+        historyApiFallback: true,
+        open: true,
+        port: 9000,
+        compress: true,
+    },
+    module:{
+        rule: [
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader , 'css-loader']
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [MiniCssExtractPlugin.loader,'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        }),
+
+        //new MiniCssExtractPlugin(),
+        
+        new PurgeCSSPlugin(),
+        new VueLoaderPlugin(),
+        new Dotenv()
+    ],
+}
+
