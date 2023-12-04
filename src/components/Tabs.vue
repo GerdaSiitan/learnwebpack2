af78dd6c3f78b23baf7f6f326c37b95186705222
GerdaSiitan <gerda.siitan13@gmail.com>
2023-12-04 08:55:32 +0200

    sdadsd

--- src/components/Tabs.vue
+++ src/components/Tabs.vue
@@ -1,25 +1,8 @@
 <template>
     <ul class="nav nav-tabs">
-        <li class="nav-item">
-            <router-link class="nav-link" to="/" activeClass="active">Home</router-link>
+        <li class="nav-item" v-for="route in $router.getRoutes()">
+            <router-link class="nav-link" :to="route.path" activeClass="active">{{ route.name }}</router-link>
         </li>
-        <li class="nav-item">
-            <router-link class="nav-link" to="/ToDo" activeClass="active">ToDo</router-link>
-        </li>
-        <li class="nav-item">
-            <router-link class="nav-link" to="/Clicker" activeClass="active">Clicker</router-link>
-        </li>
-        <li class="nav-item">
-            <router-link class="nav-link" to="/RickandMorty" activeClass="active">RickandMorty</router-link>
-        </li>
-        <li class="nav-item">
-            <router-link class="nav-link" to="/GoogleMaps" activeClass="active">googlemaps</router-link>
-        </li>
-        
-        <li class="nav-item">
-            <router-link class="nav-link" to="/Leaflet" activeClass="active">Leaflet</router-link>
-        </li>
-        
     </ul>
 </template>
 
